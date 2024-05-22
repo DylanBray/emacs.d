92dcffd0ee3e893c754905394b1b79a85078f70e
Dylan Bray <dylan@dylian.brayhouse>
2024-05-21 23:30:50 -0600

    fixed magit status binding, fixed theme

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
 
 (use-package zenburn-theme 
   :config
-  (load-theme 'zenburn))
+  (load-theme 'zenburn t))
 
 (setq
  ;; No need to see GNU agitprop.
@@ -98,7 +98,6 @@
 (use-package magit
   :diminish magit-auto-revert-mode
   :diminish auto-revert-mode
-  :bind (("C-c g" . #'magit-status))
 )
 
 
@@ -172,16 +171,4 @@
 
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
