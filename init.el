63ae5a5e6f022e0f9894e6ac269a0c52f9272c20
Dylan Bray <dylanwbray@gmail.com>
2024-09-23 16:35:00 -0600

    font

--- init.el
+++ init.el
@@ -25,6 +25,26 @@
 
 (setq gc-cons-threshold 100000000)
 
+(condition-case nil
+      ;; (set-frame-font "Iosevka SS09-12")
+      ;; (set-frame-font "Go Mono-11")
+      ;; (set-frame-font "Fira Mono-11")
+      ;; (set-frame-font "Inconsolata-13")
+      ;; (set-frame-font "Monospace-11")
+      ;; (set-frame-font "Ubuntu Mono-13")
+      ;; (set-frame-font "IBM Plex Mono-13")
+      ;; (set-frame-font "Cascadia Code-14")
+      ;; (set-frame-font "Jetbrains Mono-11")
+      ;; (set-frame-font "Monaco-11")
+         (set-frame-font "Martian Mono")
+      ;; (set-frame-font "Dina-10")
+      ;; (set-frame-font "iAWriterDuospace-13")
+      ;; (set-frame-font "Hermit-13")
+      ;; (set-frame-font "Menlo-11")
+      ;; (set-frame-font "Hack-14")
+      ;;(set-frame-font "LM Mono 10")
+    (error (set-frame-font "Monospace-11")))
+
 (use-package zenburn-theme 
   :config
   (load-theme 'zenburn t))
